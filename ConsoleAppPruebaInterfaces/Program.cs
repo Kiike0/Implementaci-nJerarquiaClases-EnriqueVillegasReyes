a143545dd3113bfabf5ca374cd1a6ab0e15250fe
Kike <enriquevilrey@gmail.com>
2023-10-17 14:23:18 +0200

    Actualización: agregadas las excepciones para que no salga de pantalla. Y un poco de mejor optimización del código y más legible.

--- ConsoleAppPruebaInterfaces/Program.cs
+++ ConsoleAppPruebaInterfaces/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
 
+public class saleDePantalla : Exception
+{
+    public saleDePantalla(String mensaje) : base ("¡Error!: "+mensaje){
+        
+    }
+}
 public interface IGrafico
 {
     public Boolean mover(int x, int y);
@@ -8,13 +15,55 @@ public interface IGrafico
 
 public class Punto : IGrafico
 {
-    public int x { get; protected set; }
-    public int y { get; protected set; }
+    private int _x;
+    public int x
+    {
+        get
+        {
+            return _x;
+        }
+        set
+        {
+            
+            if(value < 0 || value > 800)
+            {
+                throw new saleDePantalla("Se sale de pantalla (Máx. 800x600)");
+            }
+            else
+            {
+                _x = value; 
+            }
+        }
+
+
+    }
+
+    private int _y;
+    public int y {
+        get
+        {
+            return _y;
+        }
+        set
+        {
+
+            if (value < 0 || value > 800)
+            {
+                throw new saleDePantalla("Se sale de pantalla (Máx. 800x600)");
+            }
+            else
+            {
+                _y = value;
+            }
+        }
+
+    }
 
     public Punto(int x, int y)
     {
         this.x = x;
         this.y = y;
+          
     }
     public Boolean mover(int x, int y)
     {
@@ -34,26 +83,53 @@ public class Punto : IGrafico
 
 }
 
-public class Circulo : Punto
-{
-    public int radio { get; protected set; }
-
-    public Circulo(int x, int y, int radio) : base(x, y)
-    {
-        this.radio = radio;
-    }
-
-    public override String dibujar()
-    {
-        return "Círculo: Radio: " + radio + " Valores x e y: " + x + ", " + y;
-    }
-
-}
-
 public class Rectangulo : Punto
 {
-    public int ancho { get; protected set; }
-    public int alto { get; protected set; }
+    //Comprobamos que el tamaño del rectángulo no sale de pantalla
+    private int _ancho;
+    public int ancho
+    {
+        get
+        {
+            return _ancho;
+        }
+        set
+        {
+
+            if (value < 0 || value > 800)
+            {
+                throw new saleDePantalla("Se sale de pantalla (Máx. 800x600)");
+            }
+            else
+            {
+                _ancho = value;
+            }
+        }
+
+
+    }
+
+    private int _alto;
+    public int alto
+    {
+        get
+        {
+            return _alto;
+        }
+        set
+        {
+
+            if (value < 0 || value > 800)
+            {
+                throw new saleDePantalla("Se sale de pantalla (Máx. 800x600)");
+            }
+            else
+            {
+                _alto = value;
+            }
+        }
+
+    }
 
     public Rectangulo(int x, int y, int ancho, int alto) : base(x, y)
     {
@@ -64,6 +140,44 @@ public class Rectangulo : Punto
     public override String dibujar()
     {
         return "Rectángulo: Ancho: " + ancho + " Alto: " + alto + " Valores x e y: " + x + ", " + y;
+    }
+
+}
+
+public class Circulo : Punto
+{
+    //Comprobamos que el tamaño del rectángulo no sale de pantalla
+    private int _radio;
+    public int radio
+    {
+        get
+        {
+            return _radio;
+        }
+        set
+        {
+
+            if (value < 0 || value > 300)
+            {
+                throw new saleDePantalla("Se sale de pantalla (Máx. 800x600)"); //Suponemos que el radio es la mitad del tamaño vertical de la pantalla
+            }
+            else
+            {
+                _radio = value;
+            }
+        }
+
+
+    }
+
+    public Circulo(int x, int y, int radio) : base(x, y)
+    {
+        this.radio = radio;
+    }
+
+    public override String dibujar()
+    {
+        return "Círculo: Radio: " + radio + " Valores x e y: " + x + ", " + y;
     }
 
 }
@@ -114,9 +228,9 @@ public class EditorGrafico
 
     static void Main(string[] args)
     {
-        Console.WriteLine("--------------------------BIENVENIDO AL EDITOR GRAFICO-------------------------------");
+        Console.WriteLine("----------------------------BIENVENIDO AL EDITOR GRAFICO---------------------------------");
 
-        Console.WriteLine("PASO 1:");
+        Console.WriteLine("\nPASO 1:");
         GraficoCompuesto graficoCompuesto = new GraficoCompuesto();
         Console.WriteLine("Inserta los datos para crear un Gráfico Compuesto de un Punto, un Rectángulo y un Círculo");
 
@@ -126,45 +240,46 @@ public class EditorGrafico
         int puntox = 0;
         int puntoy = 0;
         Boolean puntoOk = false;
+        
 
         //Creamos un while para controlar que siga preguntando hasta que ingrese los valores correctamente
+         
+        
         while (!puntoOk)
         {
-            Console.WriteLine("Inserta el valor x del Punto: ");
-            if (int.TryParse(Console.ReadLine(), out puntox))
-            {
-                Console.WriteLine("Ingresaste el número correctamente ");
-                puntoOk = true;
-            }
-            else
-            {
-                Console.WriteLine("No ingresaste un número válido");
-                puntoOk = false;
-            }
-            Console.WriteLine("Inserta el valor y del Punto: ");
-            if (int.TryParse(Console.ReadLine(), out puntoy))
-            {
-                Console.WriteLine("Ingresaste el número correctamente ");
-                puntoOk = true;
-            }
-            else
-            {
-                Console.WriteLine("No ingresaste un número válido");
-                puntoOk = false;
-            }
-            if(puntoOk)
-            {
-                Punto puntoComprobacion = new Punto(puntox, puntoy);
-                if (!puntoComprobacion.mover(puntox, puntoy))
+            try
                 {
-                    puntoOk = false;
-                    Console.WriteLine("Error: El punto se sale de la pantalla (Máx:800x600)");
+                Console.WriteLine("Inserta el valor x del Punto: ");
+                if (int.TryParse(Console.ReadLine(), out puntox))
+                {
+                    puntoOk = true; //Comprueba si el número se ingresó correctamente
                 }
+                else
+                {
+                    Console.WriteLine("Valor x no válido.");
+                    puntoOk = false;
+                }
+                Console.WriteLine("Inserta el valor y del Punto: ");
+                if (int.TryParse(Console.ReadLine(), out puntoy))
+                {
+                    puntoOk = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor y no válido.");
+                    puntoOk = false;
+                }
+                Punto punto = new Punto(puntox, puntoy);
+                graficoCompuesto.AgregarElemento(punto);
+            } catch (saleDePantalla ex)
+            {
+               Console.WriteLine(ex.Message);
+               puntoOk=false;
             }
-            
+
         }
-        Punto punto = new Punto(puntox, puntoy);
-        graficoCompuesto.AgregarElemento(punto);
+            
+       
 
 
 
@@ -176,36 +291,39 @@ public class EditorGrafico
         Boolean rectanguloOk = false;
 
         while(!rectanguloOk) {
-            Console.WriteLine("Inserta el ancho: ");
-            if (int.TryParse(Console.ReadLine(), out ancho))
+            try
             {
-                Console.WriteLine("Ingresaste el número correctamente ");
-                rectanguloOk = true;
-            }
-            else
+                Console.WriteLine("Inserta el ancho: ");
+                if (int.TryParse(Console.ReadLine(), out ancho))
+                {
+                    rectanguloOk = true; //Comprueba si el número se ingresó correctamente
+                }
+                else
+                {
+                    Console.WriteLine("Valor del ancho no válido.");
+                    rectanguloOk = false;
+                }
+                Console.WriteLine("Inserta el alto: ");
+                if (int.TryParse(Console.ReadLine(), out alto))
+                {
+                    rectanguloOk = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor del alto no válido.");
+                    rectanguloOk = false;
+                }
+                Rectangulo rectangulo = new Rectangulo(puntox, puntoy, ancho, alto);
+                graficoCompuesto.AgregarElemento(rectangulo);
+            } catch (saleDePantalla ex)
             {
-                Console.WriteLine("No ingresaste un número válido");
-                rectanguloOk = false;
-            }
-            Console.WriteLine("Inserta el alto: ");
-            if (int.TryParse(Console.ReadLine(), out alto))
-            {
-                Console.WriteLine("Ingresaste el número correctamente ");
-                rectanguloOk = true;
-            }
-            else
-            {
-                Console.WriteLine("No ingresaste un número válido");
-                rectanguloOk = false;
-            }
-            if (ancho>800 || alto>600)
-            {
-                rectanguloOk = false;
-                Console.WriteLine("Error: El tamaño del rectángulo se sale de la pantalla (Máx:800x600)");
+                Console.WriteLine(ex.Message);
+                rectanguloOk=false;
             }
         }
-        Rectangulo rectangulo = new Rectangulo(puntox, puntoy, ancho, alto);
-        graficoCompuesto.AgregarElemento(rectangulo);
+
+            
+        
 
 
         //Insertamos los valores del Circulo para crear un Objeto Circulo para agregarlo al grafico
@@ -214,30 +332,32 @@ public class EditorGrafico
         int radio = 0;
         Boolean circuloOk = false;
 
-        while(!circuloOk)
+        while (!circuloOk)
         {
-            Console.WriteLine("Inserta el valor del radio: ");
-            if (int.TryParse(Console.ReadLine(), out radio))
+            try
             {
-                Console.WriteLine("Ingresaste el número correctamente ");
-                circuloOk = true;
-            }
-            else
+                Console.WriteLine("Inserta el valor del radio: ");
+                if (int.TryParse(Console.ReadLine(), out radio))
+                {
+                    circuloOk = true; //Comprueba si el número se ingresó correctamente
+                }
+                else
+                {
+                    Console.WriteLine("Valor del radio no válido.");
+                    circuloOk = false;
+                }
+                Circulo circulo = new Circulo(puntox, puntoy, radio);
+                graficoCompuesto.AgregarElemento(circulo);
+        } catch (saleDePantalla ex)
             {
-                Console.WriteLine("No ingresaste un número válido");
+                Console.WriteLine (ex.Message);
                 circuloOk = false;
-            }
-            if (radio> 400)
-            {
-                circuloOk = false;
-                Console.WriteLine("Error: El tamaño del círculo se sale de la pantalla (Máx:800x600)");
             }
         }
-        Circulo circulo = new Circulo(puntox, puntoy, radio);
-        graficoCompuesto.AgregarElemento(circulo);
+            
 
         //Siguiente paso: dibujar el gráfico
-        Console.WriteLine("PASO 2:");
+        Console.WriteLine("\nPASO 2:");
         Console.WriteLine("Aquí va el grafico");
         Console.WriteLine(graficoCompuesto.dibujar());
 
@@ -261,35 +381,41 @@ public class EditorGrafico
                         while (!valoresOk)
                         {
                             Console.WriteLine("Inserta el valor x:");
-                            if (int.TryParse(Console.ReadLine(), out int newX))
+                            try
                             {
-                                Console.WriteLine("Inserta el valor y:");
-                                if (int.TryParse(Console.ReadLine(), out int newY))
+                                if (int.TryParse(Console.ReadLine(), out int newX))
                                 {
-                                    if (graficoCompuesto.mover(newX, newY))
+                                    Console.WriteLine("Inserta el valor y:");
+                                    if (int.TryParse(Console.ReadLine(), out int newY))
                                     {
-                                        Console.WriteLine("Gráfico movido exitosamente.");
-                                        Console.WriteLine(graficoCompuesto.dibujar());
-                                        valoresOk = true;
+                                        if (graficoCompuesto.mover(newX, newY))
+                                        {
+                                            Console.WriteLine("Gráfico movido exitosamente.");
+                                            Console.WriteLine(graficoCompuesto.dibujar());
+                                            valoresOk = true;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("El gráfico no puede moverse fuera de la pantalla (Máximo: 800x600)");
+                                            valoresOk = false;
+                                        }
                                     }
                                     else
                                     {
-                                        Console.WriteLine("El gráfico no puede moverse fuera de la pantalla (Máximo: 800x600)");
+                                        Console.WriteLine("Valor y no válido.");
                                         valoresOk = false;
                                     }
                                 }
                                 else
                                 {
-                                    Console.WriteLine("Valor y no válido.");
+                                    Console.WriteLine("Valor x no válido.");
                                     valoresOk = false;
                                 }
-                            }
-                            else
+                            } catch (saleDePantalla ex)
                             {
-                                Console.WriteLine("Valor x no válido.");
-                                valoresOk = false;
+                                Console.WriteLine(ex.Message);
+                                valoresOk |= false;
                             }
-                            
                         }
                         break;
                     case 2:
